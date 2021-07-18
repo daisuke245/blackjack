62470279e711c527ac060aa7c558fe88cfdbc3df
daisuke245 <daisuke5220@gmail.com>
2021-07-17 22:53:05 -0500

    Creación de baraja de cartas

---
+++ assets/js/juego.js
@@ -0,0 +1,27 @@
+/*
+    2C = Two of Clubs
+    2D = Two of Diamonds
+    2H = Two of Hearts
+    2S = Two of Spades
+*/
+
+let deck            = [];
+const tipos         = ['C', 'D', 'H', 'S'];
+const especiales    = ['A', 'J', 'Q', 'K'];
+
+const crearDeck = () => {
+    for (let i = 2; i <= 10; i++){
+        for (let tipo of tipos)
+            deck.push(i + tipo);
+    }
+
+    for (let especial of especiales){
+        for (let tipo of tipos)
+            deck.push(especial + tipo);
+    }
+
+    return _.shuffle(deck);
+}
+
+deck = crearDeck();
+console.info(deck);
